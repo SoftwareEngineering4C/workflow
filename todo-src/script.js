848cd6474eed1ab65e84e4fc821a2f07cd0da48f
BuildTools <unconfigured@null.spigotmc.org>
2017-09-19 17:34:02 -0400

    Added deleteAllCompletedTasks functionality

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,13 +3,22 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];
+  $scope.todos = [
+    { task: "Learn Angular", 
+      value: false 
+    },
+    {
+      task: "Learn node",
+      value: false
+    }];
+
   $scope.newItem = "";
+
   
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
-      $scope.todos.push($scope.newItem);
+      $scope.todos.push({task: $scope.newItem, value: false});
       $scope.newItem = "";
     }
   }
@@ -20,6 +29,19 @@ myApp.controller('MainCtrl', function ($scope){
     $scope.todos.splice(index, 1);
   }
     
+  $scope.deleteAllCompletedItems = function(){
+    for (var i = 0; i < $scope.todos.length; i++)
+    {
+      console.log($scope.todos[i].value);
+
+      if ($scope.todos[i].value == true)
+      {
+        $scope.todos.splice(i, 1);
+      }
+
+    }
+    
+  }
   
 });
 
